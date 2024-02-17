370598b4ae75f9bb8b24a50e777315a0b61feb6a
tddudwns1 <tjddudwns1@naver.com>
2024-02-18 01:19:31 +0900

    🎉 프로젝트 생성
    이것저것 연습용 생성
    아마 모든 내용이 들어갈 예정
    파이탱~

---
+++ src/main/java/ssy/base/practice/BasePracticeApplication.java
@@ -0,0 +1,13 @@
+package ssy.base.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ssy/base/practice/BasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssy.base.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
